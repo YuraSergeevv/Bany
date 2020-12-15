024337d486544b8a52f676f85a02835135ddc079
YuraSergeevv <65806687+YuraSergeevv@users.noreply.github.com>
2020-12-15 15:35:10 +0200

    15:34/15.12.2020

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-$(function() {
-  $('.top__input-phone').mask('(000)000-00-00');
+$('.top__input-phone').mask('(000)000-00-00');  
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,  
+        prevArrow:'.slider__prev-arrow',
+        nextArrow: '.slider__next-arrow'
+    });
 });
-  
