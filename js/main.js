dda83f27de8b1b6db1c9f59bf737ccfc608231a3
YuraSergeevv <65806687+YuraSergeevv@users.noreply.github.com>
2020-12-16 14:59:54 +0200

    15:00/16.12.2020/2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-$('.top__input-phone').mask('(000)000-00-00');  
-
-
 $(document).ready(function(){
-    $('.slider').slick({
+
+   
+$( '.slider__inner' ).each(function(index) {
+    $('.slider', $(this)).slick({
         slidesToShow: 2,
         slidesToScroll: 2,  
-        prevArrow:'.slider__prev-arrow',
-        nextArrow: '.slider__next-arrow'
-    });
+        prevArrow:$('.slider__prev-arrow',$(this)),
+        nextArrow:$('.slider__next-arrow',$(this)),
+    })
 });
+
+$( '.last-slider__inner' ).each(function(index) {
+    $('.last-slider', $(this)).slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,  
+        prevArrow:$('.last-slider__prev-arrow',$(this)),
+        nextArrow:$('.last-slider__next-arrow',$(this)),
+    })
+});
+
+    $('.top__input-phone').mask('(000)000-00-00');  
+
+
+});
+
